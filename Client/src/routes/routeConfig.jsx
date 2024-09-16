b818f38dc86e4c9d0c68a100e14a76cc2453b047
TuanAnh <tuanmeo980provip@gmail.com>
2024-09-16 21:55:03 +0700

    Footer Tuan Meo

--- Client/src/routes/routeConfig.jsx
+++ Client/src/routes/routeConfig.jsx
@@ -1,7 +1,18 @@
 import {createBrowserRouter} from 'react-router-dom';
+import Home from "../pages/HomePage.jsx";
+import {MainLayout} from "../layouts/MainLayout.jsx";
+import {PageNotFound} from "../pages/PageNotFound.jsx";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-  }
+    path: "/" , children: [
+      {
+        element: <MainLayout/>, children: [
+          {
+            path: 'home', element: <Home/>,
+          },
+        ],
+      },
+    ] ,errorElement:<PageNotFound/>,
+  },
 ]);
