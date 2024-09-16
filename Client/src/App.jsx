ef51a946e72080d3f4a01140f644ff5ebc472e8d
TuanAnh <tuanmeo980provip@gmail.com>
2024-09-16 21:55:03 +0700

    Footer Tuan Meo

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import './index.css';
 import {RouterProvider} from 'react-router-dom';
 import {router} from './routes/routeConfig.jsx';
 
---
+++ Client/src/pages/PageNotFound.jsx
@@ -0,0 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
+export function PageNotFound() {
+    const navigate = useNavigate();
+    const handleReturnHome = () => {
+        navigate("/");
+    };
+
+    return (
+        <div className={"h-screen w-full p-10 flex justify-center items-center"}>
+            <div className={"w-full flex flex-col justify-center items-center"}>
+                <img
+                    src="/404.svg"
+                    className={"h-full w-full sm:w-2/3 md:w-1/3"}
+                    alt="404"
+                />
+                <button
+                    onClick={handleReturnHome}
+                    className={
+                        "bg-blue-500 hover:bg-blue-700 text-sm md:text-base text-white font-bold py-2 px-4 rounded-full"
+                    }
+                >
+                    Quay về trang chủ
+                </button>
+            </div>
+        </div>
+    );
+}
