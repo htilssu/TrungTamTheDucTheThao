17dc14c406bd47e929095dcfcba77dedb3021ca3
htilssu <tolashuu@gmail.com>
2024-09-17 01:34:37 +0700

    fix missing working directory when install dependency

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,10 @@
 import {RouterProvider} from 'react-router-dom';
 import {router} from './routes/routeConfig.jsx';
 
-function App({children}) {
+function App() {
 
   return (
-      <RouterProvider router={router}>
-      </RouterProvider>
+      <RouterProvider router={router}/>
   );
 }
 
