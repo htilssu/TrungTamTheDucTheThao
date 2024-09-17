26b85a9c594d05292145be87cc7fe26f302e744d
htilssu <tolashuu@gmail.com>
2024-09-18 00:06:31 +0700

    update react buddy

--- Client/src/routes/routeConfig.jsx
+++ Client/src/routes/routeConfig.jsx
@@ -2,20 +2,22 @@ import {createBrowserRouter} from 'react-router-dom';
 import Home from '../pages/HomePage.jsx';
 import {MainLayout} from '../layouts/MainLayout.jsx';
 import {PageNotFound} from '../pages/PageNotFound.jsx';
+import SignIn from '../pages/sign-in/SignIn.jsx';
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: '',
+    element: <MainLayout/>,
     children: [
       {
-        element: <MainLayout/>,
-        children: [
-          {
-            element: <Home/>,
-          },
-        ],
+        index: true,
+        element: <Home/>,
       },
     ],
     errorElement: <PageNotFound/>,
+  },
+  {
+    path: 'sign-in',
+    element: <SignIn/>,
   },
 ]);
