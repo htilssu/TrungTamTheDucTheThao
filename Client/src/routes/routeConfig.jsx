72dcd625c69ca4fc307fdfb9244089eb86d24e9a
catchmeifyoucan99 <thanhphohochiminh5@gmail.com>
2024-09-17 21:24:31 +0700

    signin

--- Client/src/routes/routeConfig.jsx
+++ Client/src/routes/routeConfig.jsx
@@ -2,7 +2,8 @@ import {createBrowserRouter} from 'react-router-dom';
 import Home from "../pages/HomePage.jsx";
 import {MainLayout} from "../layouts/MainLayout.jsx";
 import {PageNotFound} from "../pages/PageNotFound.jsx";
-import SignIn from '../modules/core/components/account/SingIn.jsx';
+import SignIn from '../modules/core/components/account/SignIn.jsx';
+
 
 export const router = createBrowserRouter([
   {
