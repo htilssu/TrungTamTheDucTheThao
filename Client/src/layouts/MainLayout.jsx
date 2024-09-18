ae0d26e30ccd974a0810e1fac8b48f58f8d9e088
Hisu <81826098+htilssu@users.noreply.github.com>
2024-09-19 00:21:54 +0700

    update deps (#25)

--- Client/src/layouts/MainLayout.jsx
+++ Client/src/layouts/MainLayout.jsx
@@ -1,21 +1,18 @@
-
-import { Outlet } from "react-router-dom";
-import {ToastContainer} from "react-toastify";
-import Footer from "../modules/core/components/Footer.jsx";
-import Navbar from "../modules/core/components/Navbar.jsx";
+import {Outlet} from 'react-router-dom';
+import {ToastContainer} from 'react-toastify';
+import Footer from '../modules/core/components/Footer.jsx';
+import Navbar from '../modules/core/components/Navbar.jsx';
 
 export function MainLayout() {
-    return (
-        <div className={"w-full"}>
-
-            <Navbar />
-
-      <div>
-        <div className={"w-full"}></div>
-        <Outlet />
+  return (
+      <div className={'w-full'}>
+        <Navbar/>
+        <div>
+          <div className={'w-full'}></div>
+          <Outlet/>
+        </div>
+        <ToastContainer/>
+        <Footer/>
       </div>
-      <ToastContainer />
-      <Footer />
-    </div>
   );
 }
