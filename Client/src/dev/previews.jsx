3fb725d5403deee9a42085bf84d2fcb74b4bca0c
Hisu <81826098+htilssu@users.noreply.github.com>
2024-09-18 14:39:59 +0700

    fix (#18)

--- Client/src/dev/previews.jsx
+++ Client/src/dev/previews.jsx
@@ -1,14 +1,10 @@
 import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox';
 import {PaletteTree} from './palette';
-import SignIn from '../pages/sign-in/SignIn.jsx';
 import {MainLayout} from '../layouts/MainLayout.jsx';
 
 const ComponentPreviews = () => {
   return (
       <Previews palette={<PaletteTree/>}>
-        <ComponentPreview path="/SignIn">
-          <SignIn/>
-        </ComponentPreview>
         <ComponentPreview path="/MainLayout">
           <MainLayout/>
         </ComponentPreview>
