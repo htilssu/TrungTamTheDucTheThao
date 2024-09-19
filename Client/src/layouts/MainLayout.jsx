6ddbca27c0a8132806075bf22795c902c04a5488
Tuan Anh <166797678+Tuan980Blue@users.noreply.github.com>
2024-09-19 22:29:06 +0700

    add navbar, home slider (#26)
    Co-authored-by: htilssu <tolashuu@gmail.com>

--- Client/src/layouts/MainLayout.jsx
+++ Client/src/layouts/MainLayout.jsx
@@ -1,18 +1,23 @@
-import {Outlet} from 'react-router-dom';
-import {ToastContainer} from 'react-toastify';
-import Footer from '../modules/core/components/Footer.jsx';
-import Navbar from '../modules/core/components/Navbar.jsx';
+
+import { Outlet } from "react-router-dom";
+import {ToastContainer} from "react-toastify";
+import Footer from "../modules/core/components/Footer.jsx";
+import Navbar from "../modules/core/system-component/Navbar.jsx";
 
 export function MainLayout() {
-  return (
-      <div className={'w-full'}>
-        <Navbar/>
-        <div>
-          <div className={'w-full'}></div>
-          <Outlet/>
+    return (
+        <div className={"w-full"}>
+
+            <div className="fixed top-0 left-0 w-full z-10">
+                <Navbar/>
+            </div>
+
+            <div className={"mt-24"}>
+                <div className={"w-full"}></div>
+                <Outlet/>
+            </div>
+            <ToastContainer/>
+            <Footer/>
         </div>
-        <ToastContainer/>
-        <Footer/>
-      </div>
-  );
+    );
 }
--- Client/src/pages/home/HomePage.jsx
+++ Client/src/pages/home/HomePage.jsx
@@ -1,15 +1,15 @@
-import SliderBanner from './components/SliderBanner.jsx';
+import ScrollableCardList from "../../modules/core/components/home/ScrollableCardList.jsx";
+import FeatureCategory from "../../modules/core/components/home/FeatureCategory.jsx";
 
 const Home = () => {
   return (
-    <div className="p-8 text-center bg-gray-100 ">
-      <SliderBanner />
-      <h1 className="text-4xl font-bold text-blue-600 mb-4">
-        Welcome to the Home Page
-      </h1>
-      <p className="text-lg text-gray-700">
-        This is the home page of our application. Feel free to explore!
-      </p>
+    <div className="p-2 sm:p-8 text-center bg-gray-100 min-h-screen">
+        <div>
+            <ScrollableCardList/>
+        </div>
+        <div>
+            <FeatureCategory/>
+        </div>
     </div>
   );
 };
