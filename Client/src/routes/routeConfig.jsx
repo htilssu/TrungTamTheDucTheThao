434cc3299755e8e400394505bbe81b7a5fcd53af
HO-LE-ANH-TOAN <143931155+HO-LE-ANH-TOAN@users.noreply.github.com>
2024-09-20 09:42:50 +0700

    feature/sign-up (#17)
    Co-authored-by: Hisu <81826098+htilssu@users.noreply.github.com>

--- Client/src/routes/routeConfig.jsx
+++ Client/src/routes/routeConfig.jsx
@@ -1,18 +1,26 @@
 import {createBrowserRouter} from 'react-router-dom';
-import Home from '../pages/home/HomePage.jsx';
-import {MainLayout} from '../layouts/MainLayout.jsx';
-import {PageNotFound} from '../pages/PageNotFound.jsx';
+import {MainLayout} from "../layouts/MainLayout.jsx";
+import {PageNotFound} from "../pages/PageNotFound.jsx";
+import SignUp from "../pages/sign-up/SignUp.jsx";
+import ForgotPassword from "../pages/forgot-password/ForgotPassword.jsx";
+import Home from "../pages/home/HomePage.jsx";
 
 export const router = createBrowserRouter([
-  {
-    path: '',
-    element: <MainLayout/>,
-    children: [
-      {
-        index: true,
-        element: <Home/>,
-      },
-    ],
-    errorElement: <PageNotFound/>,
-  },
+    {
+        path: '',
+        element: <MainLayout/>,
+        children: [
+            {
+                index: true,
+                element: <Home/>,
+            },
+        ],
+        errorElement: <PageNotFound/>,
+    },
+    {
+        path: 'sign-up', element: <SignUp/>,
+    },
+    {
+        path: 'forgot-password', element: <ForgotPassword/>,
+    },
 ]);
