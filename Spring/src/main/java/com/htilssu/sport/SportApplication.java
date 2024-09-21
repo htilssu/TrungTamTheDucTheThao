a4fb2ea6f716cfbabcc8a904365cf06010d3bc34
htilssu <tolashuu@gmail.com>
2024-09-21 13:35:19 +0700

    est-up

--- Spring/src/main/java/com/htilssu/sport/SportApplication.java
+++ Spring/src/main/java/com/htilssu/sport/SportApplication.java
@@ -2,12 +2,13 @@ package com.htilssu.sport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class SportApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SportApplication.class, args);
     }
-
 }
---
+++ Spring/src/main/java/com/htilssu/sport/configs/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.htilssu.sport.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain secureConfig(HttpSecurity http) throws Exception {
+
+
+        return http.build();
+    }
+
+}
