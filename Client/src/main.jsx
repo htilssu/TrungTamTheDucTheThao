0b25f8111a0ed1c6e01c4e8871b8a8354a28f647
htilssu <tolashuu@gmail.com>
2024-09-21 11:05:15 +0700

    config deploy client

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -5,7 +5,9 @@ import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
+export const root = createRoot(document.getElementById('root'));
+
+root.render(
     <StrictMode>
       <Suspense fallback={'Loading'}>
         <DevSupport ComponentPreviews={ComponentPreviews}
@@ -15,3 +17,5 @@ createRoot(document.getElementById('root')).render(
       </Suspense>
     </StrictMode>,
 );
+
+
--- Client/src/pages/home/HomePage.jsx
+++ Client/src/pages/home/HomePage.jsx
@@ -1,16 +1,20 @@
-import ScrollableCardList from "../../modules/core/components/home/ScrollableCardList.jsx";
-import FeatureCategory from "../../modules/core/components/home/FeatureCategory.jsx";
+import ScrollableCardList from '../../modules/core/components/home/ScrollableCardList.jsx';
+import FeatureCategory from '../../modules/core/components/home/FeatureCategory.jsx';
+import SliderBanner from './components/SliderBanner.jsx';
 
 const Home = () => {
   return (
-    <div className="p-2 sm:p-8 text-center bg-gray-100 min-h-screen">
+      <div className="p-2 sm:p-8 text-center bg-gray-100 min-h-screen">
         <div>
-            <ScrollableCardList/>
+          <SliderBanner/>
         </div>
         <div>
-            <FeatureCategory/>
+          <ScrollableCardList/>
         </div>
-    </div>
+        <div>
+          <FeatureCategory/>
+        </div>
+      </div>
   );
 };
 
