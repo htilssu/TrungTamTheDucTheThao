ab5c71ff64bbde84132cb60440c7ee2c2d92c73e
htilssu <tolashuu@gmail.com>
2024-09-21 13:39:04 +0700

    est-up

--- Client/src/routes/routeConfig.jsx
+++ Client/src/routes/routeConfig.jsx
@@ -1,26 +1,33 @@
 import {createBrowserRouter} from 'react-router-dom';
-import {MainLayout} from "../layouts/MainLayout.jsx";
-import {PageNotFound} from "../pages/PageNotFound.jsx";
-import SignUp from "../pages/sign-up/SignUp.jsx";
-import ForgotPassword from "../pages/forgot-password/ForgotPassword.jsx";
-import Home from "../pages/home/HomePage.jsx";
+import {MainLayout} from '../layouts/MainLayout.jsx';
+import {PageNotFound} from '../pages/PageNotFound.jsx';
+import SignUp from '../pages/sign-up/SignUp.jsx';
+import ForgotPassword from '../pages/forgot-password/ForgotPassword.jsx';
+import Home from '../pages/home/HomePage.jsx';
+import SignIn from '../pages/sign-in/SignIn';
 
 export const router = createBrowserRouter([
-    {
-        path: '',
-        element: <MainLayout/>,
-        children: [
-            {
-                index: true,
-                element: <Home/>,
-            },
-        ],
-        errorElement: <PageNotFound/>,
-    },
-    {
-        path: 'sign-up', element: <SignUp/>,
-    },
-    {
-        path: 'forgot-password', element: <ForgotPassword/>,
-    },
+  {
+    path: '',
+    element: <MainLayout/>,
+    children: [
+      {
+        index: true,
+        element: <Home/>,
+      },
+    ],
+    errorElement: <PageNotFound/>,
+  },
+  {
+    path: 'sign-in',
+    element: <SignIn/>,
+  },
+  {
+    path: 'sign-up',
+    element: <SignUp/>,
+  },
+  {
+    path: 'forgot-password',
+    element: <ForgotPassword/>,
+  },
 ]);
