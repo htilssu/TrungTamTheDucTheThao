d3d24614a405f2e88ad5a6ce352ba9b7530e11a7
Ho Le Anh Toan <toandong2004@gmail.com>
2024-10-08 12:43:47 +0700

    fix date

--- Client/src/modules/core/components/Sellcourses/Sellcourses.jsx
+++ Client/src/modules/core/components/Sellcourses/Sellcourses.jsx
@@ -270,6 +270,19 @@
                                 rows="4"
                             />
                         </div>
+                        <div className={"text-gray-700 font-bold mb-2"}>
+                            <label>Địa điểm</label>
+                            <input
+                                type="number"
+                                name="duration"
+                                value={formData.duration}
+                                onChange={handleChange}
+                                onKeyPress={handleKeyPress}
+                                className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                                placeholder="Nhập thời lượng khóa học"
+                                required
+                            />
+                        </div>
                     </div>
 
                     <button
