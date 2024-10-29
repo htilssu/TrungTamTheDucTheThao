edeb8ea4a9509addf58fd1fb30a43bfe1d45e365
catchmeifyoucan99 <thanhphohochiminh5@gmail.com>
2024-10-29 23:35:58 +0700

    pull

--- Spring/src/main/java/com/htilssu/sport/SportApplication.java
+++ Spring/src/main/java/com/htilssu/sport/SportApplication.java
@@ -2,10 +2,10 @@ package com.htilssu.sport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableWebSecurity
