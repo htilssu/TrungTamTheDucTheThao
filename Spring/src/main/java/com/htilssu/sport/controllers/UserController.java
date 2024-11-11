8676a9d35477768750e4f04b019f2477fdc5a5d5
Hisu <81826098+htilssu@users.noreply.github.com>
2024-11-11 23:00:02 +0700

    Feat/room (#66)
    Co-authored-by: catchmeifyoucan99 <thanhphohochiminh5@gmail.com>
    Co-authored-by: hoainghia1812 <phn4006@gmail.com>
    Co-authored-by: TuanAnh <tuanmeo980provip@gmail.com>

--- Spring/src/main/java/com/htilssu/sport/controllers/UserController.java
+++ Spring/src/main/java/com/htilssu/sport/controllers/UserController.java
@@ -1,78 +1,98 @@
 
 package com.htilssu.sport.controllers;
+
+import com.htilssu.sport.data.dtos.UserDto;
+import com.htilssu.sport.data.mappers.UserMapper;
+import com.htilssu.sport.data.models.User;
+import com.htilssu.sport.repositories.UserRepository;
+import com.htilssu.sport.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.htilssu.sport.data.models.User;
-import com.htilssu.sport.repositories.UserRepository;
-
-import jakarta.validation.Valid;
 @RestController
-@RequestMapping("/user")
+@RequestMapping("api/user")
 public class UserController {
 
     private final UserRepository userRepository;
+    private final UserService userService;
+    private final UserMapper userMapper;
 
-    public UserController(UserRepository userRepository) {
+    public UserController(UserRepository userRepository, UserService userService,
+            UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userService = userService;
+        this.userMapper = userMapper;
     }
 
     // Hiển thị thông tin người dùng
-    @CrossOrigin(origins = "http://localhost:5173") 
     @GetMapping("/{id}")
     public ResponseEntity<User> displayUserInfo(@PathVariable("id") Long id) {
-    if (id == null || id <= 0) {
-        return ResponseEntity.badRequest().build(); // Xử lý nếu ID không hợp lệ
+        if (id == null || id <= 0) {
+            return ResponseEntity.badRequest().build(); // Xử lý nếu ID không hợp lệ
+        }
+        return userRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
-    return userRepository.findById(id)
-            .map(ResponseEntity::ok)
-            .orElse(ResponseEntity.notFound().build());
-}
+
     // Sửa thông tin người dùng
     @PutMapping("/{id}")
-public ResponseEntity<?> updateUser(@PathVariable("id") Long id, @Valid @RequestBody User updatedUser, BindingResult result) {
-    if (result.hasErrors()) {
-        Map<String, String> errors = new HashMap<>();
-        result.getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
-        return ResponseEntity.badRequest().body(errors);      // Trả về các lỗi xác thực cho frontend
+    public ResponseEntity<?> updateUser(@PathVariable("id") Long id,
+            @Valid @RequestBody User updatedUser,
+            BindingResult result) {
+        if (result.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            result.getFieldErrors().forEach(
+                    error -> errors.put(error.getField(), error.getDefaultMessage()));
+            return ResponseEntity.badRequest().body(
+                    errors);      // Trả về các lỗi xác thực cho frontend
+        }
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setFirstName(
+                            updatedUser.getFirstName() != null ? updatedUser.getFirstName()
+                                                               : user.getFirstName());
+                    user.setLastName(updatedUser.getLastName() != null ? updatedUser.getLastName()
+                                                                       : user.getLastName());
+                    user.setPhoneNumber(
+                            updatedUser.getPhoneNumber() != null ? updatedUser.getPhoneNumber()
+                                                                 : user.getPhoneNumber());
+                    user.setDob(
+                            updatedUser.getDob() != null ? updatedUser.getDob() : user.getDob());
+                    user.setGender(updatedUser.getGender() != null ? updatedUser.getGender()
+                                                                   : user.getGender());
+                    user.setAvatar(updatedUser.getAvatar() != null ? updatedUser.getAvatar()
+                                                                   : user.getAvatar());
+                    userRepository.save(user);
+                    return ResponseEntity.ok(user);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
-    return userRepository.findById(id)
-            .map(user -> {
-                user.setFirstName(updatedUser.getFirstName() != null ? updatedUser.getFirstName() : user.getFirstName());
-                user.setLastName(updatedUser.getLastName() != null ? updatedUser.getLastName() : user.getLastName());
-                user.setPhoneNumber(updatedUser.getPhoneNumber() != null ? updatedUser.getPhoneNumber() : user.getPhoneNumber());
-                user.setDob(updatedUser.getDob() != null ? updatedUser.getDob() : user.getDob());
-                user.setGender(updatedUser.getGender() != null ? updatedUser.getGender() : user.getGender());
-                user.setAvatar(updatedUser.getAvatar() != null ? updatedUser.getAvatar() : user.getAvatar());
-                userRepository.save(user);
-                return ResponseEntity.ok(user);
-            })
-            .orElse(ResponseEntity.notFound().build());
-}
+
     // Tạo người dùng mới
     @PostMapping
-public ResponseEntity<?> createUser(@Valid @RequestBody User user, BindingResult result) {
-    if (result.hasErrors()) {
-        Map<String, String> errors = new HashMap<>();
-        result.getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
-        System.out.println("Validation errors: " + result.getFieldErrors());
-        return ResponseEntity.badRequest().body(errors); 
+    public ResponseEntity<?> createUser(@Valid @RequestBody User user, BindingResult result) {
+        if (result.hasErrors()) {
+            Map<String, String> errors = new HashMap<>();
+            result.getFieldErrors().forEach(
+                    error -> errors.put(error.getField(), error.getDefaultMessage()));
+            System.out.println("Validation errors: " + result.getFieldErrors());
+            return ResponseEntity.badRequest().body(errors);
+        }
+        // Kiểm tra tuổi trước khi lưu
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
-       // Kiểm tra tuổi trước khi lưu
-    User savedUser = userRepository.save(user);
-    return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
-}
-    
+
+    @GetMapping("/me")
+    public UserDto getMe(Authentication authentication) {
+        return userService.getUser(authentication);
+    }
 }
