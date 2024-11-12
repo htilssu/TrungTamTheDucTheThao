c82c9d8d0d892386c9d3628cd37c5bd898ce364c
htilssu <tolashuu@gmail.com>
2024-11-12 23:12:36 +0700

    fix: user token

--- Spring/src/main/java/com/htilssu/sport/exceptions/GlobalExceptionHandler.java
+++ Spring/src/main/java/com/htilssu/sport/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.htilssu.sport.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -9,8 +10,6 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
-        // Log chi tiết lỗi
-        System.out.println(e.getMessage());
-        return new ResponseEntity<>("Đã xảy ra lỗi hệ thống", HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
