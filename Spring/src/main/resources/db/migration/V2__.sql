0afa5eaa66998445607c1e0f65342809b72695b7
catchmeifyoucan99 <thanhphohochiminh5@gmail.com>
2024-11-13 00:33:43 +0700

    test push

--- Spring/src/main/resources/db/migration/V2__.sql
+++ Spring/src/main/resources/db/migration/V2__.sql
@@ -1,7 +1,7 @@
 ALTER TABLE coach
 ADD COLUMN experience INT,      
 ADD COLUMN rating DECIMAL(3, 2),  
-ADD COLUMN description VARCHAR(500);
+ADD COLUMN description VARCHAR(400);
 
 ALTER TABLE course
 ADD COLUMN type VARCHAR(50) NOT NULL, 
